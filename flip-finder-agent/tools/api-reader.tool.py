4a81150b884721b313eba1d6d9b9d37c55bd4143
ChristianHesberg <cchesberg@gmail.com>
2024-12-01 10:34:27 +0100

    Initial commit

---
+++ flip-finder-agent/tools/api-reader.tool.py
@@ -0,0 +1,20 @@
+﻿import requests
+
+def getTimeSeriesById(id):
+    r = requests.get('https://prices.runescape.wiki/api/v1/osrs/timeseries?timestep=5m&id=' + str(id),
+                     headers={'Accept': 'application/json',
+                              'User-Agent': 'I am trying to create a program to quickly find good margins on items that I specify.',
+                              'From': 'hesbergc@yandex.com'
+                              })
+    return r.json()
+
+def getItemById(id):
+    r = requests.get('https://prices.runescape.wiki/api/v1/osrs/latest?id=' + str(id),
+                     headers={'Accept': 'application/json',
+                              'User-Agent': 'I am trying to create a program to quickly find good margins on items that I specify.',
+                              'From': 'hesbergc@yandex.com'
+                              })
+    return r.json()
+
+print(getTimeSeriesById(13237))
+print(getItemById(13237))

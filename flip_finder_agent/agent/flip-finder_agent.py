ec644e98a91f0eaf4d366d012ff2e3e7dff972df
ChristianHesberg <90753431+ChristianHesberg@users.noreply.github.com>
2024-12-02 16:32:25 +0100

    Add discord bot tool (#2)
    * create basic discord bot tool
    
    * we gamin

--- flip_finder_agent/agent/flip-finder_agent.py
+++ flip_finder_agent/agent/flip-finder_agent.py
@@ -1,5 +1,12 @@
-﻿from autogen import ConversableAgent
+﻿import asyncio
+import os
+
+import discord
+from autogen import ConversableAgent
+from dotenv import load_dotenv
+
 from flip_finder_agent.config import LLM_CONFIG
+from flip_finder_agent.tools.discord_bot_tool import send_flip
 from flip_finder_agent.tools.item_list_tool import item_list
 from flip_finder_agent.tools.api_reader_tool import getData
 from flip_finder_agent.tools.flip_finder_tool import find_flip
@@ -25,7 +32,7 @@ def create_flip_finder_agent() -> ConversableAgent:
     agent.register_for_llm(name="item_list", description="Read item list")(item_list)
     agent.register_for_llm(name="api_reader", description="Read item data")(getData)
     agent.register_for_llm(name="flip_finder", description="Analyze if a trade on an item is safe")(find_flip)
-
+    agent.register_for_llm(name="discord_bot", description="Sends message to discord with flip")(send_flip)
     return agent
 
 def create_user_proxy():
@@ -38,6 +45,7 @@ def create_user_proxy():
     user_proxy.register_for_execution(name="item_list")(item_list)
     user_proxy.register_for_execution(name="api_reader")(getData)
     user_proxy.register_for_execution(name="flip_finder")(find_flip)
+    user_proxy.register_for_execution(name="discord_bot")(send_flip)
     return user_proxy
 
 
@@ -50,13 +58,14 @@ def main():
                 1. Read items from the item list, using the item_list tool.
                 2. For each item, fetch the data of the item using the api_reader tool.
                 3. For each item, determine the safety of trading the item using the flip_finder tool.
-                4. Create a JSON object that contains the item id and the analyzed flip information needed for making the trade.
+                4. Create a stringified JSON object that contains the item id and the analyzed flip information needed for making the trade.
                 Example:
                 [
                     {"item_id": "1", "safe": false, "margin": 10000, "buy_price": 100000, "sell_price": 111000},
                     {"item_id": "2", "safe": true, "margin": 20000, "buy_price": 200000, "sell_price": 222000},
                 ]
-                5. Return the JSON object.
+                5. Send the stringified JSON object to discord using the discord_bot send_flip method.
+                6. Return the JSON object.
                 """
     )
 
---
+++ flip_finder_agent/tools/discord_bot_tool.py
@@ -0,0 +1,11 @@
+﻿import requests
+
+
+def send_flip(text: str):
+    url = 'http://127.0.0.1:5000'
+    payload = {
+        'message': text
+    }
+    requests.post(url, json=payload)
+
+send_flip('a dumb lame message')
